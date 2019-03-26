80850edb617fa38e5dfbfd8f707e6e0be6a18305
Chris Pernicano <chris.pernicano@gmail.com>
2019-03-26 12:55:18 -0600

    update windows to 5.2.2

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'poise-python', '~> 1.7.1'
-depends 'windows', '~> 3.1.3'
+depends 'windows', '~> 5.2.2'
 
 %w{ ubuntu debian centos redhat fedora amazon windows}.each do |os|
   supports os
