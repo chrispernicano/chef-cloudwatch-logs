1fdc2eaa52be4babead67a00a985f5b9075b9e28
Chris Pernicano <chris.pernicano@gmail.com>
2019-03-26 12:53:31 -0600

    update poise-python to 1.7.1

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs and configures AWS CloudWatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'poise-python', '~> 1.6.0'
+depends 'poise-python', '~> 1.7.1'
 depends 'windows', '~> 3.1.3'
 
 %w{ ubuntu debian centos redhat fedora amazon windows}.each do |os|
